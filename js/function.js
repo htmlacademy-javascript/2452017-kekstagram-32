c455e723eceb4a88494c77b03d90a73da1a4f850
Элдар Саркынов <sarkynoveldar@gmail.com>
2024-07-13 00:18:05 +0300

    Функции возвращаются

--- js/function.js
+++ js/function.js
@@ -24,3 +24,31 @@ console.log(checkStringLength("Длинна строки больше, пока�
 console.log(isPalindrome("топот"));
 console.log(isPalindrome("ДовОд"));
 console.log(isPalindrome("Кекс"));
+
+
+
+// Функция "Делу - время"
+
+const checkWorkTime = function  (Start, End, Meeting, timeMeeting) {
+
+    const workStart = Start.split (':');
+    const workEnd = End.split (':');
+    const meetingStart = Meeting.split (':');
+
+    const totalStartMinutes = parseInt(workStart[0] * 60) + parseInt(workStart [1]);
+    const totalEndMinutes = parseInt (workEnd[0] * 60) + parseInt(workEnd[1]);
+    const totalMeetingMinutes = parseInt (meetingStart[0] * 60) + parseInt(meetingStart[1]);
+
+    if ((totalMeetingMinutes >=totalStartMinutes) && (totalMeetingMinutes + timeMeeting) <= totalEndMinutes){
+
+     return true
+    }
+return false;
+}
+
+checkWorkTime ('08:00', '17:30', '14:00', 90)
+checkWorkTime ('8:0', '10:0', '8:0', 120)
+checkWorkTime ('08:00', '14:30', '14:00', 90)
+checkWorkTime ('14:00', '17:30', '08:0', 90)
+checkWorkTime ('8:00', '17:30', '08:00', 900)
+
